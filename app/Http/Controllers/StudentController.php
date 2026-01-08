106560fad79fac4f734b3fe0f6de306f0d2c9576
Fardin Ahmed <fardinahmed7443@gmail.com>
2026-01-08 14:15:28 +0600

    updated

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    function student_dashboard() {
+        return "Student Dashboard";
+    }
+
+    function addStudent() {
+        return "Add Student";
+    }
+
+    function viewStudent() {
+        return "View Student";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('home');
+});
+
+// Route::get('/student/dashboard', [StudentController::class, 'student_dashboard']);
+// Route::get('/student/add-student', [StudentController::class, 'addStudent']);
+// Route::get('/student/view-student', [StudentController::class, 'viewStudent']);
+
+Route::prefix('student')->group(function () {
+    Route::get('/dashboard', [StudentController::class, 'student_dashboard']);
+    Route::get('/add-student', [StudentController::class, 'addStudent']);
+    Route::get('/view-student', [StudentController::class, 'viewStudent']);
 });
