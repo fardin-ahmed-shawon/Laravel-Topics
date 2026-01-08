8ed1f96474f09b1a2289925002bdb8c09d90f69e
Fardin Ahmed <fardinahmed7443@gmail.com>
2026-01-08 15:53:16 +0600

    updated

--- resources/views/student.blade.php
+++
@@ -1,1 +0,0 @@
-<h1>Welcome To Student Panel</h1>
--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,3 @@ use App\Http\Controllers\StudentController;
 Route::get('/', function () {
     return view('home');
 });
-
-
-// Route grouping with controller
-// Route::controller(StudentController::class)->group(function () {
-//     Route::get('student/dashboard', 'student_dashboard');
-//     Route::get('student/add-student', 'addStudent');
-//     Route::get('student/view-student', 'viewStudent');
-// });
-
-
-// Route grouping with controller & prefix
-Route::controller(StudentController::class)->prefix('student')->group(function () {
-    Route::get('/dashboard', 'student_dashboard');
-    Route::get('/add-student', 'addStudent');
-    Route::get('/view-student/{name}', 'viewStudent');
-});
