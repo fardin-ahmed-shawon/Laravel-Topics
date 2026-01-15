bf06a93249122ce4d4745ad704dc4fc640557f8d
Fardin Ahmed <fardinahmed7443@gmail.com>
2026-01-15 17:26:03 +0600

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     function getUsers() {
+
+        $data = new \App\Models\User;
+        echo $data->getUserId();
+
         $users ='\App\Models\User'::all();
-        return $users;
+        return view('users', ['users' => $users]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    
+
+    // If a different table name using for User Model then
+    // protected $table = 'd-users';
+
+    function getUserId() {
+        return "User ID will return";
+    }
+
 }
